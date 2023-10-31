3944b1ca63eda8565daba90dc876e0affea2b669
ayoubwazane <ayoubwazane306@gmail.com>
2023-10-31 12:55:08 +0100

    submitting updates

--- NQ/testing.py
+++ NQ/testing.py
@@ -8,7 +8,7 @@ from datetime import datetime, timedelta, date
 pd.options.display.float_format = "{:,.4f}".format
 
 # Inputs and Parameters
-filename = "qqq_quotedata.csv"
+filename = "Vqqq_quotedata.csv"
 
 
 # Black-Scholes European-Options Gamma
